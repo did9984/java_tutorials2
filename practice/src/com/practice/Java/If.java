a66f9bc74d9cc2d35105189e4c5ec50e432c4957
did9984 <113024115+did9984@users.noreply.github.com>
2023-01-10 14:29:31 +0900

    practice

---
+++ practice/src/com/practice/Java/If.java
@@ -0,0 +1,16 @@
+package com.practice.Java;
+
+public class If {
+ public static void main(String[] args) {
+	int age = 18;
+	if (age>=18) {
+		System.out.println("your are adults");
+	}
+	else if(age==75) {
+			System.out.println("you are boomer!");
+		}
+	else {
+		System.out.println("you are too young");
+	}
+}
+}
---
+++ practice/src/com/practice/Java/Switch_practice.java
@@ -0,0 +1,33 @@
+package com.practice.Java;
+
+public class Switch_practice {
+   public static void main(String[] args) {
+  String day ="ay";
+  switch (day) {
+case "sunday":
+	System.out.println("It is Sunday");
+	break;
+case "monday":
+	System.out.println("It is monday");
+	break;
+case "tuesday":
+	System.out.println("It is tuesday");
+	break;
+case "wednesday":
+	System.out.println("It is wednesday");
+	break;
+case "Thursday":
+	System.out.println("It is Thursday");
+	break;
+case "Firday":
+	System.out.println("It is Friday");
+	break;
+case "Saturday":
+	System.out.println("It is Saturday");
+	break;
+default:
+	System.out.println("This is not day");
+	break;
+}
+}
+}
