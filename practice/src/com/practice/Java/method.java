b4f308c80fdcf167e3a5c405712985e394b44482
did9984 <113024115+did9984@users.noreply.github.com>
2023-01-12 14:33:43 +0900

    practice

--- practice/src/com/practice/Java/method.java
+++ practice/src/com/practice/Java/method.java
@@ -5,9 +5,10 @@ public class method {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-  hello("park");
+		hello("park");
 	}
-static void hello(String na1) {
-	System.out.println("hello "+na1);
-}
+
+	static void hello(String na1) {
+		System.out.println("hello " + na1);
+	}
 }
